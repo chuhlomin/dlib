fbd82c981d9fc6c21e40095b68c10e6fd3c215a9
Konstantin Chukhlomin <konstantin.chukhlomin@nytimes.com>
2014-10-25 12:43:57 +0400

    Landing page

--- dlib/views.py
+++ dlib/views.py
@@ -3,13 +3,23 @@ from django.shortcuts import render_to_response
 
 
 def landing(request):
-    return render_to_response('landing.html')
+    images = [
+        'cryptonomicon.jpg',
+        'erlang.jpg',
+        'hgttg.jpg',
+        'thinking.jpg'
+    ]
+
+    return render_to_response('landing.html', {'images': images})
+
 
 def user(request):
     return render_to_response('user.html')
 
+
 def book(request):
     return render_to_response('book.html')
+
 
 def booklist(request):
     return render_to_response('booklist.html')
