1321b349e59123ff73445b08a93cb9fc9564d325
Pavel Grafov <Pavel_Grafov@epam.com>
2014-10-25 08:42:14 -0400

    Borrow on Admin

--- dlib/admin.py
+++ dlib/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.sites.models import Site
-from models import Book
+from models import Book, Borrow
 
 admin.site.unregister(Site)
 admin.site.register(Book)
+admin.site.register(Borrow)
